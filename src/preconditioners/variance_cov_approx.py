499b7c2f1c484744e2b4da5db4d2db3d86d66d81
Eduard Oravkin <eduard.oravkin@gmail.com>
2022-03-14 18:49:00 +0000

    progress on covariance approximation

--- src/preconditioners/variance_cov_approx.py
+++ src/preconditioners/variance_cov_approx.py
@@ -2,7 +2,12 @@ from __future__ import division, print_function, absolute_import
 
 import numpy as np
 
+# P = C.dot(C.T)
+# X = X
+# B = Sigma (cov_empir)
+
 def fAndG(B, C, X, a):
+    # this one uses the log barrier
     assert isinstance(B, np.ndarray)
     dim = B.shape
     assert len(dim) == 2
@@ -33,6 +38,54 @@ def fAndG(B, C, X, a):
     t_7 = (1 / (10 ** 10))
     functionValue = (np.trace(((((((((T_0).dot(X)).dot(C)).dot(C.T)).dot(B)).dot(C)).dot(C.T)).dot(X.T)).dot(T_0)) - (a * np.log((t_7 + t_6))))
     gradient = (((((2 * T_2) - (T_4 + T_3)) + (2 * (((((((B).dot(C)).dot(C.T)).dot(X.T)).dot(T_0)).dot(T_0)).dot(X)).dot(C))) - (T_3 + T_4)) - (((2 * a) / (t_6 + t_7)) * ((np.linalg.det(T_5) * np.linalg.inv(T_5))).dot(C)))
+
+    return functionValue, gradient
+
+def invbarrier(C):
+    assert isinstance(C, np.ndarray)
+    dim = C.shape
+    assert len(dim) == 2
+    C_rows = dim[0]
+    C_cols = dim[1]
+
+    T_0 = (C).dot(C.T)
+    t_1 = np.linalg.det(T_0)
+    functionValue = (1 / t_1)
+    #gradient = -((2 / (t_1 ** 2)) * ((np.linalg.det(T_0) * np.linalg.inv(T_0))).dot(C))
+    gradient = -((2 / t_1) * (np.linalg.inv(T_0)).dot(C))
+    return functionValue, gradient
+
+def fAndG_invbarrier(B, C, X, a):
+    # this one uses the 1/x barrier
+    assert isinstance(B, np.ndarray)
+    dim = B.shape
+    assert len(dim) == 2
+    B_rows = dim[0]
+    B_cols = dim[1]
+    assert isinstance(C, np.ndarray)
+    dim = C.shape
+    assert len(dim) == 2
+    C_rows = dim[0]
+    C_cols = dim[1]
+    assert isinstance(X, np.ndarray)
+    dim = X.shape
+    assert len(dim) == 2
+    X_rows = dim[0]
+    X_cols = dim[1]
+    if isinstance(a, np.ndarray):
+        dim = a.shape
+        assert dim == (1, )
+    assert B_cols == C_rows == X_cols == B_rows
+
+    T_0 = np.linalg.inv((((X).dot(C)).dot(C.T)).dot(X.T))
+    T_1 = (X.T).dot(T_0)
+    T_2 = ((((((T_1).dot(T_0)).dot(X)).dot(C)).dot(C.T)).dot(B)).dot(C)
+    T_3 = (((((((((((T_1).dot(X)).dot(C)).dot(C.T)).dot(B)).dot(C)).dot(C.T)).dot(X.T)).dot(T_0)).dot(T_0)).dot(X)).dot(C)
+    T_4 = (((((T_2).dot(C.T)).dot(X.T)).dot(T_0)).dot(X)).dot(C)
+
+    invbarrier_val, invbarrier_grad = invbarrier(C)
+    functionValue = (np.trace(((((((((T_0).dot(X)).dot(C)).dot(C.T)).dot(B)).dot(C)).dot(C.T)).dot(X.T)).dot(T_0)) + a * invbarrier_val)
+    gradient = (((((2 * T_2) - (T_4 + T_3)) + (2 * (((((((B).dot(C)).dot(C.T)).dot(X.T)).dot(T_0)).dot(T_0)).dot(X)).dot(C))) - (T_3 + T_4)) + a * invbarrier_grad)
 
     return functionValue, gradient
 
