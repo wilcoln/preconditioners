f0cb86ab1a99e7e6557ef906e61d107cbf9fc5ea
Eduard Oravkin <eduard.oravkin@gmail.com>
2022-03-15 17:20:19 +0000

    small fix

--- src/preconditioners/tests/test_p_inv.py
+++ src/preconditioners/tests/test_p_inv.py
@@ -3,7 +3,7 @@ import unittest
 from torch import nn
 from torch.utils.data import random_split
 
-import settings
+from preconditioners import settings
 from preconditioners.datasets import CenteredGaussianDataset
 from preconditioners.cov_approx.impl_cov_approx import *
 from preconditioners.utils import generate_c, generate_centered_gaussian_data
@@ -15,14 +15,13 @@ class TestPinv(unittest.TestCase):
     def test_p_inv(self):
 
         d = 30
+        train_size = 10
+        test_size = 2
+        extra_size = 1000
         c = generate_c(ro=.5, regime='autoregressive', d=d)
 
         w_star = np.random.multivariate_normal(mean=np.zeros(d), cov=np.eye(d))
-        dataset = CenteredGaussianDataset(w_star=w_star, d=d, c=c, n=1000)
-        
-        train_size = int(0.7 * len(dataset))
-        test_size = int(0.2 * len(dataset))
-        extra_size = len(dataset) - train_size - test_size
+        dataset = CenteredGaussianDataset(w_star=w_star, d=d, c=c, n = train_size + test_size + extra_size)
 
         train_dataset, test_dataset, extra_dataset = random_split(dataset, [train_size, test_size, extra_size])
 
@@ -49,5 +48,6 @@ class TestPinv(unittest.TestCase):
             np.linalg.norm(p_inv - np.linalg.inv(c)) < 0.01,
             msg=f'The error is {np.linalg.norm(p_inv - np.linalg.inv(c))} and \
             the first entries of p_inv are {p_inv[:4,:4]}\
-            while the first entries of the true matrix are {np.linalg.inv(c)[:4,:4]}'
+            while the first entries of the true matrix are {np.linalg.inv(c)[:4,:4]}\
+            and the first entries of X^TX/n are {((dataset.X).T @ (dataset.X)/(train_size+test_size+extra_size))[:4,:4]}'
         )
